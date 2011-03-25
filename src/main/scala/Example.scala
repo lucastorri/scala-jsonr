b9fe0dd02cef824c34c2b8f827f30bdb910ac4a9
Lucas Torri <lucastorri@gmail.com>
2011-03-25 10:46:13 -0300

    putting quotes in strings of an array

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,4 +1,4 @@
-import _root_.co.torri.jsonr._
+import co.torri.jsonr._
 
 object Example {
     def main(args: Array[String]) = {
@@ -7,6 +7,7 @@ object Example {
           "key"   -> "value",
           "other" -> 2,
           "array" -> %(1,2,3,4,5),
+          "strgs" -> %("a", "b", "c"),
           "more"  -> %(
             $(
               "foo" -> "bar"
@@ -21,6 +22,8 @@ object Example {
         )
         
         println(json)
-        
+      
     }
 }
+
+
--- src/main/scala/co/torri/jsonr.scala
+++ src/main/scala/co/torri/jsonr.scala
@@ -1,21 +1,23 @@
 package co.torri
 
 package object jsonr {
+    
+    private def jsonizeString(a: Any) = a match {
+        case s: String => format(""" "%s" """, s).trim //trim: just because it breaks the TextMate color theme
+        case a => a.toString
+    }
 
     class JSONBlock(inner: List[(String, Any)]) {
         override def toString = {
             "{" + inner.map { t =>
-                val value =  t._2 match {
-                    case s: String => format(""" "%s" """, s).trim //trim: just because it breaks the TextMate color theme
-                    case a => a.toString
-                }
+                val value = jsonizeString(t._2)
                 format(""""%s": %s""", t._1, value)
             }.mkString(", ") + "}"
         }
     }
     class JSONArray(el: List[Any]) {
         override def toString = {
-            "[" + el.mkString(", ") + "]"
+            "[" + el.map(jsonizeString).mkString(", ") + "]"
         }
     }
 
