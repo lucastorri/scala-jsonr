6ba4a4e8ac168a139bb609ea0326728845349cbb
Lucas Torri <lucastorri@gmail.com>
2011-03-29 19:09:20 -0300

    Putting scalatest as a test resource

--- project/build/Proj.scala
+++ project/build/Proj.scala
@@ -2,6 +2,6 @@ import sbt._
 
 class Proj(info: ProjectInfo) extends DefaultProject(info) {
 
-    val scalatest = "org.scalatest" % "scalatest" % "1.3"
+    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
 
 }
