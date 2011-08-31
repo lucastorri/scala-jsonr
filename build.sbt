71ad538bce5ef62c60050bf2eb24cc98b34e3573
Lucas Torri <lucastorri@gmail.com>
2011-08-31 12:02:13 -0300

    moving to xsbt

---
+++ build.sbt
@@ -0,0 +1,7 @@
+organization := "co.torri"
+
+name := "scala-jsonr"
+
+version := "1.0"
+
+scalaVersion := "2.9.0-1"
--- project/build/Proj.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-class Proj(info: ProjectInfo) extends DefaultProject(info) {
-
-    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
-
-}
